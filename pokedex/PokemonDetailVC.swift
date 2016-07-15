acda7c7258940a78f3432ab466e04ee99852de30
Adam Goth <adamgoth@gmail.com>
2016-07-15 07:16:10 -0600

    Adds detail view controller outlets and new fields to Pokemon class

--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -12,6 +12,28 @@ class PokemonDetailVC: UIViewController {
     
     @IBOutlet weak var nameLbl: UILabel!
     
+    @IBOutlet weak var mainImg: UIImageView!
+    
+    @IBOutlet weak var descriptionLbl: UILabel!
+    
+    @IBOutlet weak var typeLbl: UILabel!
+    
+    @IBOutlet weak var defenseLbl: UILabel!
+    
+    @IBOutlet weak var heightLbl: UIStackView!
+    
+    @IBOutlet weak var pokedexLbl: UIStackView!
+    
+    @IBOutlet weak var weightLbl: UIStackView!
+    
+    @IBOutlet weak var baseAttackLbl: UIStackView!
+    
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
+    @IBOutlet weak var evoLbl: UILabel!
+    
     var pokemon: Pokemon!
 
     override func viewDidLoad() {
@@ -20,4 +42,7 @@ class PokemonDetailVC: UIViewController {
         nameLbl.text = pokemon.name
     }
 
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
