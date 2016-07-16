330d495ad4f763ed6ff7a6af6ee20d05e6b8396d
Adam Goth <adamgoth@gmail.com>
2016-07-16 07:52:43 -0600

    Parses JSON for all necessary data

---
+++ pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Adam Goth on 7/15/16.
+//  Copyright © 2016 Adam Goth. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -11,27 +11,16 @@ import UIKit
 class PokemonDetailVC: UIViewController {
     
     @IBOutlet weak var nameLbl: UILabel!
-    
     @IBOutlet weak var mainImg: UIImageView!
-    
     @IBOutlet weak var descriptionLbl: UILabel!
-    
     @IBOutlet weak var typeLbl: UILabel!
-    
     @IBOutlet weak var defenseLbl: UILabel!
-    
     @IBOutlet weak var heightLbl: UIStackView!
-    
     @IBOutlet weak var pokedexLbl: UIStackView!
-    
     @IBOutlet weak var weightLbl: UIStackView!
-    
     @IBOutlet weak var baseAttackLbl: UIStackView!
-    
     @IBOutlet weak var currentEvoImg: UIImageView!
-    
     @IBOutlet weak var nextEvoImg: UIImageView!
-    
     @IBOutlet weak var evoLbl: UILabel!
     
     var pokemon: Pokemon!
@@ -40,6 +29,11 @@ class PokemonDetailVC: UIViewController {
         super.viewDidLoad()
 
         nameLbl.text = pokemon.name
+        mainImg.image = UIImage(named: "\(pokemon.pokedexId)")
+        
+        pokemon.downloadPokemonDetails { () -> () in
+            //called when download completes
+        }
     }
 
     @IBAction func backBtnPressed(sender: AnyObject) {
